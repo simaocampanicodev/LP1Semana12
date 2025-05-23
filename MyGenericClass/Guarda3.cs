98d65754205fe42f7a09f70c05e6699186121836
Simão <simaocampanico.dev@gmail.com>
2025-05-23 01:40:50 +0100

    Exercise 3, Guarda3 class in project MyGenericClass

---
+++ MyGenericClass/Guarda3.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Guarda3<T>
+{
+    private T item0;
+    private T item1;
+    private T item2;
+
+    public Guarda3()
+    {
+        item0 = default(T);
+        item1 = default(T);
+        item2 = default(T);
+    }
+
+    public T GetItem(int i)
+    {
+        return i switch
+        {
+            0 => item0,
+            1 => item1,
+            2 => item2,
+            _ => throw new IndexOutOfRangeException("Índice inválido: " + i)
+        };
+    }
+
+    public void SetItem(int i, T item)
+    {
+        switch (i)
+        {
+            case 0: item0 = item; break;
+            case 1: item1 = item; break;
+            case 2: item2 = item; break;
+            default: throw new IndexOutOfRangeException("Índice inválido: " + i);
+        }
+    }
+}
---
+++ MyGenericClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var guardaStrings = new Guarda3<string>();
+        guardaStrings.SetItem(0, "Hello");
+        guardaStrings.SetItem(1, "World");
+        guardaStrings.SetItem(2, "!");
+        Console.WriteLine($"{guardaStrings.GetItem(0)} {guardaStrings.GetItem(1)}{guardaStrings.GetItem(2)}");
+        
+        var guardaFloats = new Guarda3<float>();
+        guardaFloats.SetItem(0, 1.1f);
+        guardaFloats.SetItem(1, 2.2f);
+        guardaFloats.SetItem(2, 3.3f);
+        Console.WriteLine($"{guardaFloats.GetItem(0)}, {guardaFloats.GetItem(1)}, {guardaFloats.GetItem(2)}");
+    }
+}
