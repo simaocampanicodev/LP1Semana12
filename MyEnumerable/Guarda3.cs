2e811072233aebd136e2f7eef8ec5d27d6d0e39e
Simão <simaocampanico.dev@gmail.com>
2025-05-23 01:46:59 +0100

    Exercise 4, Guarda3 class again and using foreach in the last exercise. MyEnumerable project

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Guarda3<T> : IEnumerable<T>
+{
+    private T item0;
+    private T item1;
+    private T item2;
+
+    public Guarda3()
+    {
+        item0 = default(T);
+        item1 = default(T);
+        item2 = default(T);
+    }
+
+    public T GetItem(int i)
+    {
+        return i switch
+        {
+            0 => item0,
+            1 => item1,
+            2 => item2,
+            _ => throw new IndexOutOfRangeException("Índice inválido: " + i)
+        };
+    }
+
+    public void SetItem(int i, T item)
+    {
+        switch (i)
+        {
+            case 0: item0 = item; break;
+            case 1: item1 = item; break;
+            case 2: item2 = item; break;
+            default: throw new IndexOutOfRangeException("Índice inválido: " + i);
+        }
+    }
+    
+    public IEnumerator<T> GetEnumerator()
+    {
+        yield return item0;
+        yield return item1;
+        yield return item2;
+    }
+    
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ MyEnumerable/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var guardaStrings = new Guarda3<string>();
+        guardaStrings.SetItem(0, "Hello");
+        guardaStrings.SetItem(1, "World");
+        guardaStrings.SetItem(2, "!");
+        
+        Console.WriteLine("--- Strings ---");
+        Console.WriteLine($"{guardaStrings.GetItem(0)} {guardaStrings.GetItem(1)}{guardaStrings.GetItem(2)}");
+        
+        foreach (string item in guardaStrings)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        
+        var guardaFloats = new Guarda3<float>();
+        guardaFloats.SetItem(0, 1.1f);
+        guardaFloats.SetItem(1, 2.2f);
+        guardaFloats.SetItem(2, 3.3f);
+        
+        Console.WriteLine("--- Floats ---");
+        Console.WriteLine($"{guardaFloats.GetItem(0)}, {guardaFloats.GetItem(1)}, {guardaFloats.GetItem(2)}");
+        
+        foreach (float item in guardaFloats)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+        
+        var guardaInts = new Guarda3<int>();
+        guardaInts.SetItem(0, 10);
+        guardaInts.SetItem(1, 20);
+        guardaInts.SetItem(2, 30);
+        
+        Console.WriteLine("-- Inteiros ---");
+        foreach (int item in guardaInts)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
